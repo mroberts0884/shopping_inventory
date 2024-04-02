dfbf012af6a7b34ba732a9cafc078a2098a45d3d
Matt Roberts <mroberts0884@gmail.com>
2024-04-01 23:21:54 -0400

    Added kivy stuff

--- shopping_inv.py
+++ shopping_inv.py
@@ -1,10 +1,29 @@
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.app  import App
 import sqlite3
-import tkinter as ttk
 
-#Create inventory database
-con = sqlite3.connect("inventory.db")
-cur = con.cursor()
+class shoppingInv(Widget):
+    pass
 
-#Create Table in Database
-cur.execute("CREATE TABLE Shopping_List(Item, Quantity)")
+class Shopping_inv(App):
+    def build(self):
+        #Connect to Database
+        conn = sqlite3.connect("inventory.db")
+
+        #Create A Cursor
+        cur = conn.cursor()
+
+        #Create Table
+        cur.execute("CREATE TABLE if not exists Shopping_List(Item, Quantity)")
+        
+        #Commit changes
+        conn.commit()
+
+        #Close our connection
+        conn.close
+        return shoppingInv
+
+if __name__=="__main__":
+    Shopping_inv().run()
 
