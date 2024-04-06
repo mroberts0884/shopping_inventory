4ce4daa2fed1e262916c576656c58bc00c2a7268
Matt Roberts <mroberts0884@gmail.com>
2024-04-06 00:12:44 -0400

    functionality added to the buttons

--- shoppinginv.py
+++ shoppinginv.py
@@ -25,31 +25,55 @@ class Shoppinginv(App):
         #Close our connection
         conn.close
 
-        def add(self):
-            
-            item = self.root.ids.item_input
-            quantity = self.root.ids.quantity_input
-               #Connect to Database
-            conn = sqlite3.connect("inventory.db")
-
-            #Create A Cursor
-            cur = conn.cursor()
-
-           
-            #Add Item and Quantity
-            cur.execute("INSERT INTO Shopping_List(Item, Quantity) VALUES(?,?)", item, quantity)
-
-            #clear input
-            self.root.ids.item_input = ''
-            self.root.ids.quantity_input = ''
-
-            #Commit changes
-            conn.commit()
-
-            #Close our connection
-            conn.close
-
         return shoppingInv()
+
+    def add(self):
+        item = self.root.ids.item_input
+        quantity = self.root.ids.quantity_input
+        #Connect to Database
+        conn = sqlite3.connect("inventory.db")
+
+        #Create A Cursor
+        cur = conn.cursor()
+
+        #Add Item and Quantity
+        cur.execute("INSERT INTO Shopping_List(Item, Quantity) VALUES(?,?)", item, quantity)
+
+        #clear input
+        self.root.ids.item_input.text = ''
+        self.root.ids.quantity_input.text = ''
+
+        #Commit changes
+        conn.commit()
+
+        #Close our connection
+        conn.close      
+
+    def generate_list(self):
+             
+        #Connect to Database
+        conn = sqlite3.connect("inventory.db")
+
+        #Create A Cursor
+        cur = conn.cursor()
+        
+        #Grab item from shopping list
+        cur.execute("SELECT Item from Shopping_List WHERE Quantity = 0")
+        shop_list = cur.fetchall()
+
+        word = ''
+        #Loop thru items
+        for item in shop_list:
+            word = f"{word}\n{item[0]}"
+            self.root.ids.list.text = f"{word}"
+        
+        #Commit changes
+        #conn.commit()
+
+        #Close our connection
+        conn.close
+        
+        
 
 if __name__=="__main__":
     Shoppinginv().run()
